c00a0613f3c9fedee86c3271e93607ea6c9b9a43
th_01 <andrey.denissov@gmail.com>
2022-05-21 21:00:56 +0300

    build

--- src/main/java/com/anden/learn/kubernetes/App.java
+++ src/main/java/com/anden/learn/kubernetes/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.anden.learn.kubernetes.controller"})
 public class App {
     public static void main(String... args) {
-        System.out.println("Hi");
+        System.out.println("Hi there!");
         SpringApplication.run(App.class, args);
     }
 }
